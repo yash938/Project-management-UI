1184ee724f8bd4e64de78e67cdfeef04f36731a9
Yash Sharma <yash32860@gmail.com>
2024-12-04 22:26:39 +0530

    some changes in project detils

--- src/pages/ProjectDetails/IssueCard.jsx
+++ src/pages/ProjectDetails/IssueCard.jsx
@@ -6,7 +6,7 @@ const IssueCard = () => {
     
     <Card classNme="rounded-md py-1 pb-2">
         <CardHeader classNme="py-0 pb-1">
-            <div className='flex justify-between items-center'></div>
+            <div className='flex justify-between items-center'>2:37</div>
         </CardHeader>
     </Card>  
     
